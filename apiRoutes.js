b604a88faa0f15dd79b0429be45c6e3e7ff164d7
ctsalva <coreysalva@gmail.com>
2020-03-05 21:00:10 -0500

    Updated api routes

--- apiRoutes.js
+++ apiRoutes.js
@@ -16,31 +16,36 @@ module.exports = function (app) {
     // In each of the below cases the user is shown an HTML page of content
     // ---------------------------------------------------------------------------
 
+
+
+    app.get("/api/waitlist", function (req, res) {
+        return res.json(waitingArray);
+    });
+
+    app.get("/api/reservations", function (req, res) {
+        return res.json(tableArray);
+    });
+
+    // If no matching route is found default to home
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "./index.html"));
+    });
+
+
     //HTML POST Requests
 
     app.post("/api/reservations", function (req, res) {
         var newRes = req.body;
         if (tableArray.length === 5) {
             waitingArray.push(newRes);
+            newRes.hasReservation = false;
         }
         else {
             tableArray.push(newRes);
+            newRes.hasReservation = true;
         }
 
         res.json(newRes);
 
-    });
-
-    app.get("/api/waitlist", function (req, res) {
-        return res.json(waitingArray);
-    });
-
-    app.get("/reservations", function (req, res) {
-        res.sendFile(path.join(__dirname, "./reservations.html"));
-    });
-
-    // If no matching route is found default to home
-    app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "./index.html"));
     });
 };
